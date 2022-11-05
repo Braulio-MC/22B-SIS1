942e4cba425cec2bef7a3fa5916650444cf75a41
Braulio Martínez Carrillo <braulio.martinez1138@alumnos.udg.mx>
2022-11-05 15:00:23 -0600

    Basic implementation of react-router-dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,26 @@
 import LoginForm from "./components/loginform"
 import Menu from "./components/Menu"
+import { Routes, Route } from "react-router-dom"
+
+/**
+ * NOTA
+ * <Route path="*" element={''} />: 
+ * Esta ruta es para enlaces que no existen
+ * Cambia '' por el componente que quieres que se visualice cuando intenten acceder a ese enlace
+ * Y esa ruta siempre es la ultima
+ */
 
 function App() {
   return (
     <div>
         <Menu/>
-    </div>
-  );
+        <Routes>
+          <Route exact path="/login" element={<LoginForm />} />
+          //? Aqui van los enlaces que quieras crear
+          <Route path="*" element={''} />
+        </Routes>                               
+    </div>                                      
+  );                                        
 }
 
 export default App;
